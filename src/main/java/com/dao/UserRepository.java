d3a1ee73e8730ddfd801110ab01ceeb012a2da04
gottamsnehamary <61041030+gottamsnehamary@users.noreply.github.com>
2020-04-24 12:31:36 +0530

    day 2 complete

---
+++ src/main/java/com/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.dao;
+
+ import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+public List<User> findAll();
+}
